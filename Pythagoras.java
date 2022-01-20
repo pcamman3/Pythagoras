c1309b8530f42cdc7aa16081349f6037f351c77a
pcamman3 <w0467387@campus.nscc.ca>
2022-01-20 18:54:26 -0400

    Final working program

--- Pythagoras.java
+++ Pythagoras.java
@@ -1,6 +1,23 @@
+import java.lang.Math;
+
 public class Pythagoras {
 	public static void main(String []args) {
-		System.out.println("A right angle triangle has 3 sides. Sides a = 20cm, b = 30cm, c = ?")
+		System.out.println("A right angle triangle has 3 sides. Sides a = 20cm, b = 30cm, c = ?");
+		System.out.println("Calculating a^2 + b^2 = c^2");
+		
+		int output;
+		double a = 20;
+		double b = 30;
+		double c;
+			
+		c = calculateHypotenuse(a, b);
+		
+		System.out.println("The answer is: " + c);
+	}
+	
+	public static double calculateHypotenuse(double a, double b) {
+		double output = Math.sqrt((a * a) + (b * b));
+		return output;
 	}
 	
 }
